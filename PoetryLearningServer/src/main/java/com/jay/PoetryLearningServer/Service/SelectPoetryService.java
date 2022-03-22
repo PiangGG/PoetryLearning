6b646c4a3a4c6af33baa5e0e6881faefc81e3699
PiangGG <974805475@qq.com>
2022-03-22 23:17:34 +0800

    修改服务器获取诗词列表方式,完成UE客户端诗词列表

--- PoetryLearningServer/src/main/java/com/jay/PoetryLearningServer/Service/SelectPoetryService.java
+++ PoetryLearningServer/src/main/java/com/jay/PoetryLearningServer/Service/SelectPoetryService.java
@@ -6,8 +6,11 @@ import com.jay.PoetryLearningServer.entity.PoetryExample;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.Array;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Vector;
 
 @Service
 public class SelectPoetryService {
@@ -20,11 +23,14 @@ public class SelectPoetryService {
         PoetryExample poetryExample=new PoetryExample();
 
         Map map = new HashMap();
+
+        Vector poetryArr=new Vector();
         for (int i=0;i<poetryMapper.countByExample(poetryExample);i++)
         {
-            map.put(i,poetryMapper.selectByPrimaryKey(i));
+            //map.put(i,poetryMapper.selectByPrimaryKey(i));
+            poetryArr.add(poetryMapper.selectByPrimaryKey(i));
         }
-
+        map.put("Array",poetryArr);
         return map;
     }
 }
